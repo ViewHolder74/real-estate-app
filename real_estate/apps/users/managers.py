c4b07522e352f74ed2580967e13dc1ee1b7096db
ViewHolder74 <viewholder74@gmail.com>
2022-06-14 01:58:02 +0200

    feat:add profile model and signals

--- real_estate/apps/users/managers.py
+++ real_estate/apps/users/managers.py
@@ -11,59 +11,59 @@ class CustomeUserManager(BaseUserManager):
         except ValidationError:
             raise ValueError(_('You must provide a valid email address'))
         
-        def create_user(self,username,firstname,last_name,email,password, **extra_fields):
-            if not username:
-                raise ValueError(_('Users must submit a username'))
+    def create_user(self,username,firstname,last_name,email,password, **extra_fields):
+        if not username:
+            raise ValueError(_('Users must submit a username'))
             
-            if not firstname:
-                raise ValueError(_('Usesrs must submit a fist name'))
+        if not firstname:
+            raise ValueError(_('Usesrs must submit a fist name'))
             
-            if not last_name:
-                raise ValueError(_('Users must submit last name'))
+        if not last_name:
+            raise ValueError(_('Users must submit last name'))
             
-            if email:
-                email = self.normalize_email(email)
-                self.email_validator(email)
-            else:
-                raise ValueError(_('Base User Account: An email address is required'))
+        if email:
+            email = self.normalize_email(email)
+            self.email_validator(email)
+        else:
+            raise ValueError(_('Base User Account: An email address is required'))
             
-            user = self.model(
-                username=username,
-                firstname=firstname,
-                last_name=last_name,
-                email=email,
-                **extra_fields
-            )
+        user = self.model(
+            username=username,
+            firstname=firstname,
+            last_name=last_name,
+            email=email,
+            **extra_fields
+        )
             
-            user.set_password(password)
-            extra_fields.setdefault('is_staff', False)
-            extra_fields.setdefault('is_superuser', False)
-            user.save(using=self._db)
-            return user 
+        user.set_password(password)
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        user.save(using=self._db)
+        return user 
         
-        def create_superuser(self,username,firstname,last_name,email,password, **extra_fields):
-            extra_fields.setdefault('is_staff', True)
-            extra_fields.setdefault('is_superuser', True)
-            extra_fields.setdefault('is_active', True)
+    def create_superuser(self,username,firstname,last_name,email,password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
             
-            if extra_fields.get('is_staff') is not True:
-                raise ValueError(_('Superusers must have is_staff=True'))
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superusers must have is_staff=True'))
             
-            if extra_fields.get('is_superuser') is not True:
-                raise ValueError(_('Superusers must have is_superuser=True'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superusers must have is_superuser=True'))
             
-            if not password:
-                raise ValueError(_('Superusers must have password'))
+        if not password:
+            raise ValueError(_('Superusers must have password'))
             
-            if email:
-                email = self.normalize_email(email)
-                self.email_validator(email)
-            else:
-                raise ValueError(_('Admin Account: An email address is required'))
+        if email:
+            email = self.normalize_email(email)
+            self.email_validator(email)
+        else:
+            raise ValueError(_('Admin Account: An email address is required'))
             
-            user = self.create_user(username,firstname,last_name,password, **extra_fields)
-            user.save(using=self._db)
-            return user
+        user = self.create_user(username,firstname,last_name,email,password, **extra_fields)
+        user.save(using=self._db)
+        return user
             
                 
                 
